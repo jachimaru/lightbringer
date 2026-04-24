840c179925d4456ceff9240a04c3f75dc0e82415
jacob <jachimaru@gmail.com>
2026-04-23 22:35:57 -0400

    update bee

--- scripts/bee.gd
+++ scripts/bee.gd
@@ -13,31 +13,33 @@ const JUMP_VELOCITY = -400.0
 @onready var attack_ray_cast: ShapeCast2D = $AttackRayCast
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 
-var direction = 0
+var direction = 1
+var start_position: Vector2
+var move_timer: float = 0.0
+
+func _ready() -> void:
+	start_position = position
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
 	if not is_on_floor():
 		sprite.play("idle")
-	
-	set_move_route(delta)
-	#move_and_slide()
 
-func set_move_route(delta):
-	if direction != -1:
-		position.x += move_distance * delta * direction
-		print("position: ", position.x)
-		await get_tree().create_timer(move_time).timeout
-		print("timer up")
-		direction = -1
-		print("direction: ", direction)
-	if direction == -1:
-		position.x += move_distance * delta * direction
-		print("position: ", position.x)
-		await get_tree().create_timer(move_time).timeout
-		print("timer up")
-		direction = 1
-		print("direction: ", direction)
+	set_move_route(delta)
+
+func set_move_route(delta: float) -> void:
+	if move_time <= 0.0:
+		return
+
+	move_timer += delta
+	while move_timer >= move_time:
+		move_timer -= move_time
+		direction *= -1
+
+	var progress := move_timer / move_time
+	if direction == 1:
+		position.x = start_position.x + move_distance * progress
+	else:
+		position.x = start_position.x + move_distance * (1.0 - progress)
 
 #func detect_light(delta):
 	#var collider = light_detector.get_collider()
