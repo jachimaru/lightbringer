005114abd9ace8dcaa458a1c838e8f443a51da97
jacob <jachimaru@gmail.com>
2026-05-12 22:42:19 -0400

    Level 4

---
+++ scripts/hidden_tiles.gd
@@ -0,0 +1,22 @@
+extends TileMapLayer
+
+@onready var tiles: TileMapLayer = $"."
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	animation_player.play("fade_out")
+	print("in area")
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	animation_player.play("fade_in")
+	print("out area")
--- scripts/snail.gd
+++ scripts/snail.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
+const SPEED = 450.0
 const JUMP_VELOCITY = -200.0
 
 @onready var snail_sprite: AnimatedSprite2D = $SnailSprite
